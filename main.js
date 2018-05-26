4c691c624fd177fcf5dd88e7b88075518b649f65
Xavier Campenon <x.campenon@gmail.com>
2018-05-26 13:10:26 +0200

    better pizza dialog

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     window.Drolles = {};
 
     Drolles.scrollToTarget = function (event, target) {
-        var elTarget = window.document.querySelector('#' + target);
+        var elTarget = window.document.getElementById(target);
 
         if (elTarget.scrollIntoView) {
             elTarget.scrollIntoView({ behavior: 'smooth' });
@@ -12,4 +12,30 @@
         event.stopPropagation();
         return false;
     };
+
+    Drolles.openDialog = function (event, target) {
+        closeDialog();
+        
+        var dialogContent = window.document.getElementById('pizza-' + target).innerHTML;
+
+        var dialog = document.createElement("div");
+        dialog.setAttribute('id', 'dialog');
+        dialog.setAttribute('class', 'blackboard');
+        dialog.innerHTML = dialogContent;
+        
+        window.document.querySelector('body').appendChild(dialog);
+
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    };
+    
+    window.onclick = closeDialog;
+    
+    function closeDialog() {
+        var dialog = window.document.getElementById('dialog');
+        if (dialog) {
+            dialog.remove();
+        }
+    }
 })(window);
