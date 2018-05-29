71b5ca996214e6d2613a2f59367aced0c9c17161
Xavier Campenon <x.campenon@gmail.com>
2018-05-29 21:30:27 +0200

    click fixes for Safari mobile

--- main.js
+++ main.js
@@ -2,41 +2,40 @@
     window.Drolles = {};
 
     Drolles.scrollToTarget = function (event, target) {
+        closeDialog();
+        
         var elTarget = window.document.getElementById(target);
 
         if (elTarget.scrollIntoView) {
             elTarget.scrollIntoView({ behavior: 'smooth' });
+            
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
         }
-
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
     };
 
     Drolles.openDialog = function (event, target) {
-        closeDialog();
-        
-        var dialogContent = window.document.getElementById('pizza-' + target).innerHTML;
+        if (!closeDialog()) {
+            var dialogContent = window.document.getElementById('pizza-' + target).innerHTML;
 
-        var dialog = document.createElement("div");
-        dialog.setAttribute('id', 'dialog');
-        dialog.setAttribute('class', 'blackboard');
-        dialog.innerHTML = dialogContent;
+            var dialog = document.createElement("div");
+            dialog.setAttribute('id', 'dialog');
+            dialog.setAttribute('class', 'blackboard');
+            dialog.innerHTML = dialogContent;
         
-        window.document.querySelector('body').appendChild(dialog);
+            window.document.querySelector('body').appendChild(dialog);
 
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        }
     };
     
     window.onclick = closeDialog;
-    // window.ontouchend = closeDialog;
     
     function closeDialog() {
         var dialog = window.document.getElementById('dialog');
-        if (dialog) {
-            dialog.remove();
-        }
+        return dialog && !dialog.remove();
     }
 })(window);
